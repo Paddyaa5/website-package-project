27590b0b154b114f360a4c7961041530f9a3fef2
PaddyAA <Patrick_aanderson@hotmail.co.uk>
2021-06-18 10:32:54 +0100

    added section components, state, amended app.js

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -11,14 +11,24 @@ export const GlobalStyle = createGlobalStyle`
 
 body{
     overscroll-behavior-y: none;
+    font-family: 'Open Sans', sans-serif;
 }
 a, button{
     outline: none;
     text-decoration: none;
     cursor: pointer;
 }
-/* h1,h2,h3{
-    font-family: "Potta One", cursive;
-} */
+h1,h2,h3{
+    font-weight: 800
+}
+h4,h5{
+    font-weight: 600
+}
+p{
+    font-weight: 400
+}
+h6{
+    font-weight: 300
+}
 
 `;
